dc9b3abca91c7fde36128235cc21ad0f4b65898a
Avinash Maurya <akmaurya31@gmail.com>
2021-01-30 07:38:30 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/wms',
+    MONGODB_URL: process.env.MONGODB_URL || 'mon1godb://loc1alhost11/wms',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
