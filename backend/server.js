a7a6745d5a39bd55e94528479274b8d61333f427
Avinash Maurya <akmaurya31@gmail.com>
2021-02-01 17:11:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,14 +27,6 @@ const mongodbUrl= config.MONGODB_URL;
 console.log("I am mongodbUrflfffffffffffff", mongodbUrl);
 
 
-      useMongoClient: true,
-      autoIndex: false, // Don't build indexes
-      reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-      reconnectInterval: 500, // Reconnect every 500ms
-      poolSize: 10, // Maintain up to 10 socket connections
-      // If not connected, return errors immediately rather than waiting for reconnect
-      bufferMaxEntries: 0
-
 mongoose.connect(mongodbUrl, {
 	useNewUrlParser:true,
 	useUnifiedTopology: true,
