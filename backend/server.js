18f400baff63eb1261b3bb66703c4d4dcbaccc1d
Avinash Maurya <akmaurya31@gmail.com>
2021-02-01 17:10:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,11 +27,25 @@ const mongodbUrl= config.MONGODB_URL;
 console.log("I am mongodbUrflfffffffffffff", mongodbUrl);
 
 
+      useMongoClient: true,
+      autoIndex: false, // Don't build indexes
+      reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+      reconnectInterval: 500, // Reconnect every 500ms
+      poolSize: 10, // Maintain up to 10 socket connections
+      // If not connected, return errors immediately rather than waiting for reconnect
+      bufferMaxEntries: 0
 
 mongoose.connect(mongodbUrl, {
 	useNewUrlParser:true,
 	useUnifiedTopology: true,
-	useCreateIndex:true
+	//useCreateIndex:true,
+	autoIndex: false, // Don't build indexes
+	useMongoClient: true,
+	reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+	reconnectInterval: 500, // Reconnect every 500ms
+	poolSize: 10, // Maintain up to 10 socket connections
+        // If not connected, return errors immediately rather than waiting for reconnect
+	bufferMaxEntries: 0
 }).catch(error => console.log(error.reason));
 
 const app=express();
